58d15d829905f03a8313683a57036de420b63f9b
nkubana0 <i.shema@alustudent.com>
2023-11-06 02:12:14 -0800

    advanced api

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-0-main
+function that queries the Reddit API
+returns the number of subscribers (not active users, total subscribers) for a given subreddit. 
+If an invalid subreddit is given, the function should return 0
 """
 import sys
 
