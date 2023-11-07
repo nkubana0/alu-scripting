21c5b5d49692b1f70949f229fb90bdf87c6f047b
nkubana0 <i.shema@alustudent.com>
2023-11-07 00:10:19 -0800

    api_advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,42 +1,12 @@
 #!/usr/bin/python3
 """
-This script defines a function to retrieve the number of subscribers for a subreddit from the Reddit API.
-It also provides a command-line interface for using this function.
-
-Author: Your Name
-
-Usage:
-    python3 reddit_subscribers.py <subreddit_name>
-
-Arguments:
-    <subreddit_name> - The name of the subreddit for which you want to retrieve the number of subscribers.
-
-Example:
-    python3 reddit_subscribers.py programming
-
-Dependencies:
-    - You need the 'requests' module installed to make HTTP requests. You can install it using 'pip install requests'.
-
-Note:
-    - Make sure you have the 'requests' module installed.
-    - You need to pass the name of the subreddit as a command-line argument.
-
+function that return the number of subscribers from a REDDIT API 
 """
 
-import sys
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Retrieve the number of subscribers for a subreddit from the Reddit API.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for the given subreddit, or 0 if the subreddit does not exist.
-
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyRedditBot/1.0 (by YourUsername)"}
 
@@ -49,9 +19,12 @@ def number_of_subscribers(subreddit):
     else:
         return 0
 
+
 if __name__ == "__main__":
+    import sys
     if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+        print("Please pass an argument")
+
     else:
         subreddit = sys.argv[1]
         num_subscribers = number_of_subscribers(subreddit)
