bd5ee5966842999f1cb01341bdfd3d5514771c4d
nkubanashift <shemaivan27@gmail.com>
2023-11-12 19:44:24 +0200

    count

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,51 +1,58 @@
 #!/usr/bin/python3
 """
-function that queries the Reddit API
-parses the title of all hot articles
-prints a sorted count of given keywords
+Script that queries the Reddit API,
+parses the title of hot articles in a subreddit,
+and prints a sorted count of given keywords.
+
 """
 
 import requests
 
+def count_words(subreddit, word_list, after=None, results=None):
+    """
+    Count occurrences of keywords in the titles of hot articles in a subreddit.
 
-def count_words(subreddit, word_list, counts=None, after=None):
-    if counts is None:
-        counts = {}
+    """
+    if results is None:
+        results = {}
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
-    headers = {'User-agent': 'myRedditScript/1.0'}
-    params = {'after': after} if after else {}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'limit': 10, 'after': after} if after else {'limit': 10}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    # Make a request to the Reddit API
+    response = requests.get(url, params=params, headers={'User-Agent': 'YOUR_USER_AGENT'})
 
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            new_posts = data['data']['children']
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(f"Error: HTTP Status Code {response.status_code}")
+        return
 
-            for post in new_posts:
-                title = post['data']['title']
-                for word in word_list:
-                    count = title.lower().count(word.lower())
-                    counts[word.lower()] += count
+    # Parse JSON response
+    data = response.json()
 
-            after = data['data']['after']
-            if after:
-                return count_words(subreddit, word_list, counts, after)
-            else:
-                sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-                for word, count in sorted_counts:
-                    if count > 0:
-                        print(f"{word}: {count}")
-        else:
-            print("No posts found")
+    # Process the data if it has the expected structure
+    if 'data' in data and 'children' in data['data']:
+        for child in data['data']['children']:
+            title_words = child['data']['title'].lower().split()
+
+            for word in word_list:
+                if word.lower() in title_words:
+                    results[word.lower()] = results.get(word.lower(), 0) + title_words.count(word.lower())
+
+        # Call the function recursively with the next set of hot articles
+        count_words(subreddit, word_list, after=data['data'].get('after'), results=results)
+
     else:
-        print("Invalid subreddit or no posts match")
+        print("Error: Unexpected response format")
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        count_words(sys.argv[1], [x.lower() for x in sys.argv[2].split()])
+    if results:
+        # Sort and print the results
+        sorted_results = sorted(results.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_results:
+            print(f"{word}: {count}")
+
+# Example usage:
+if __name__ == "__main__":
+    subreddit_name = 'python'
+    keywords = ['python', 'java', 'javascript']
+    count_words(subreddit_name, keywords)
